2e280d640941a4eba03741e89dd21635f3255f89
mrprst <prost.mathias@gmail.com>
2021-11-29 16:55:23 +0100

    stripe

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root to: "pictures#index"
   devise_for :users
-  resources :orders
   resources :tags
   resources :users
-  resources :pictures
+  resources :pictures do
+    resources :orders, only: [:new, :create]
+end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
