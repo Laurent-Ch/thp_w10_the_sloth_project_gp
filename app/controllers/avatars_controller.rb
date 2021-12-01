ce97e630e5b157b2be36b5830a0860c39d608008
VioMrqs <violette.marquis@gmail.com>
2021-12-01 12:15:19 +0100

    avatar ok, display better

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -1,5 +1,9 @@
 class AvatarsController < ApplicationController
-  @user = User.find(params[:user_id])
-  @user.avatar.attach(params[:avatar])
-  redirect_to(user_path(@user))
+
+  def create
+    @user = User.find(params[:user_id])
+    @user.avatar.attach(params[:avatar])
+    redirect_to(user_path(@user))
+  end
+  
 end
